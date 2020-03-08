c4df4020b9692f83236abe56bd612e3e28493908
randypj92 <randy_li25@hotmail.com>
2020-03-08 10:22:00 -0400

    Initial Commit

---
+++ WPFBootstrapUI/BootstrapUISample/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace BootstrapUISample
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPFBootstrapUI/WPFBootstrapUI/src/Controls/Alert/Implementation/Alert.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WPFBootstrapUI.src.Controls.Alert.Implementation
+{
+    public class Alert : ContentControl
+    {
+        static Alert()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Alert), new FrameworkPropertyMetadata(typeof(Alert)));
+        }
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(Alert), new PropertyMetadata(string.Empty));
+
+
+        public static CornerRadius GetCornerRadius(DependencyObject obj)
+        {
+            return (CornerRadius)obj.GetValue(CornerRadiusProperty);
+        }
+
+        public static void SetCornerRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(Alert), new PropertyMetadata(new CornerRadius(2.0)));
+
+
+
+
+
+
+
+        public Brush AlertForeground
+        {
+            get { return (Brush)GetValue(AlertForegroundProperty); }
+            set { SetValue(AlertForegroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AlertForeground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AlertForegroundProperty =
+            DependencyProperty.Register("AlertForeground", typeof(Brush), typeof(Alert), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0,0,0))));
+
+
+
+
+
+
+    }
+}
