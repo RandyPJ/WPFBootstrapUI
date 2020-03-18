ec4877024b0868f058a4542aa3179244f0f39e33
randypj92 <randy_li25@hotmail.com>
2020-03-18 06:41:39 -0400

    LinkButton -> (Ready)

---
+++ WPFBootstrapUI/WPFBootstrapUI/src/TextAssist.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WPFBootstrapUI.src
+{
+    public static class TextAssist
+    {
+        public static string GetText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(TextProperty);
+        }
+
+        public static void SetText(DependencyObject obj, string value)
+        {
+            obj.SetValue(TextProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.RegisterAttached("Text", typeof(string), typeof(TextAssist), new PropertyMetadata(string.Empty));
+    }
+}
