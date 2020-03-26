3d1347870c47abc07842e95740eb8f84949ac1df
randypj92 <randy_li25@hotmail.com>
2020-03-26 16:58:39 -0400

    No important change

---
+++ WPFBootstrapUI/WPFBootstrapUI/src/Controls/TextBox/Implementation/TextBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFBootstrapUI.src.Controls
+{
+    public class TextBox : ContentControl
+    {
+        static TextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBox), new FrameworkPropertyMetadata(typeof(TextBox)));
+        }
+    }
+}
