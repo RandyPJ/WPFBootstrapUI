a920fef7be777f7ee31e1628c5be28bc4c4955df
randypj92 <randy_li25@hotmail.com>
2020-03-29 16:47:39 -0400

    No importance changes

--- WPFBootstrapUI/WPFBootstrapUI/src/Controls/Badge/Implementation/Badge.cs
+++ WPFBootstrapUI/WPFBootstrapUI/src/Controls/Badge/Implementation/Badge.cs
@@ -15,6 +15,7 @@ namespace WPFBootstrapUI.src.Controls
             get { return (string)GetValue(TextProperty); }
             set { SetValue(TextProperty, value); }
         }
+        
 
         // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TextProperty =
