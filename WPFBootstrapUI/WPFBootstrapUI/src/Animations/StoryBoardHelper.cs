4d2b2b5354f40d793bdc885219fd247b441417e7
randypj92 <randy_li25@hotmail.com>
2020-03-29 11:34:17 -0400

    New Class: StoryboardHelper
    This class contains extension methods for animations, like: slide to left, slide to right and so on.

---
+++ WPFBootstrapUI/WPFBootstrapUI/src/Animations/StoryBoardHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace WPFBootstrapUI.src.Animations
+{
+    public static class StoryBoardHelper
+    {
+        public static void SlideToLeft(this Storyboard storyboard, long seconds, double accelerationRatio)
+        {
+            Storyboard sb = new Storyboard();
+
+            new ThicknessAnimation()
+            {
+                Duration = new Duration(new TimeSpan(seconds)),
+                From = new Thickness(),
+                AccelerationRatio = accelerationRatio
+            };
+        }
+    }
+}
