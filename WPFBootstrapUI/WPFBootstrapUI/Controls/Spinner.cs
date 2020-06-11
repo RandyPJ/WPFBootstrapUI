0cedea679d3cee1ff62c1ea4509a8ef02da21d53
randypj92 <randy_li25@hotmail.com>
2020-06-11 15:54:16 -0400

    New Control: Spinner.

---
+++ WPFBootstrapUI/WPFBootstrapUI/Controls/Spinner.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFBootstrapUI.Controls
+{
+    public class Spinner: Control
+    {
+        static Spinner()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Spinner), new FrameworkPropertyMetadata(typeof(Spinner)));
+        }
+
+        public static readonly DependencyProperty DoSpinProperty =
+           DependencyProperty.Register("DoSpin", typeof(bool), typeof(Spinner), new PropertyMetadata(false));
+
+        
+        public static readonly DependencyProperty StrokeThicknessProperty =
+           DependencyProperty.Register("StrokeThickness", typeof(double), typeof(Spinner), new PropertyMetadata(0d));
+
+        public bool DoSpin
+        {
+            get => (bool)GetValue(DoSpinProperty);
+            set => SetValue(DoSpinProperty, value);
+        }
+
+        public double StrokeThickness
+        {
+            get { return (double)GetValue(StrokeThicknessProperty); }
+            set { SetValue(StrokeThicknessProperty, value); }
+        }
+    }
+}
