9daa9178a9f910c56a0fd987f644acdd208342b9
randypj92 <randy_li25@hotmail.com>
2020-08-31 22:03:01 -0400

    Mejoras

--- WPFBootstrapUI/BootstrapUISample/App.xaml.cs
+++ WPFBootstrapUI/BootstrapUISample/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace BootstrapUISample
 {
@@ -13,5 +16,6 @@ namespace BootstrapUISample
     /// </summary>
     public partial class App : Application
     {
+        
     }
 }
