0cfb368f321219f69e4d38e5190ad0e832ca74d4
Randy <randy_li25@hotmail.com>
2020-09-19 09:28:06 -0400

    Revision y eliminacion de fuentes innecesarias

--- WPFBootstrapUI/WPFBootstrapUI/Controls/Badge.cs
+++ WPFBootstrapUI/WPFBootstrapUI/Controls/Badge.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using WPFBootstrapUI.WPFBootstrapColors;
 
 namespace WPFBootstrapUI.Controls
 {
---
+++ WPFBootstrapUI/WPFBootstrapUI/WPFBootstrapColors/BootstrapColors.cs
@@ -0,0 +1,17 @@
+﻿namespace WPFBootstrapUI.WPFBootstrapColors
+{
+    public enum BootstrapColor
+    {
+        Primary,
+        Secondary,
+        Success,
+        Danger,
+        Warning,
+        Info,
+        Light,
+        Dark,
+        Muted,
+        White,
+        Gray
+    }
+}
