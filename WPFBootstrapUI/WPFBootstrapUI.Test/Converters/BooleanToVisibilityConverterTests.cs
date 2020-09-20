7cc9f7c6bc716054129383c3ac39ecdbacd96227
randypj92 <randy_li25@hotmail.com>
2020-09-20 19:16:35 -0400

    unit test project added.
    BooleanToVisibilityConverter passed the tests

---
+++ WPFBootstrapUI/WPFBootstrapUI.Test/Converters/BooleanToVisibilityConverterTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFBootstrapUI.Converters.Tests
+{
+    [TestClass()]
+    public class BooleanToVisibilityConverterTests
+    {
+        [TestMethod()]
+        public void ConvertToVisibleTest()
+        {
+            BooleanToVisibilityConverter converter = new BooleanToVisibilityConverter();
+
+            Assert.AreEqual(converter.Convert(true, typeof(FrameworkElement), null, null), Visibility.Visible);
+        }
+
+        [TestMethod()]
+        public void ConvertCollapseTest()
+        {
+            BooleanToVisibilityConverter converter = new BooleanToVisibilityConverter();
+            Assert.AreEqual(converter.Convert(false, typeof(FrameworkElement), null, null), Visibility.Collapsed);
+        }
+
+        [TestMethod()]
+        public void ConvertBackTest()
+        {
+            BooleanToVisibilityConverter converter = new BooleanToVisibilityConverter();
+            Assert.AreEqual(converter.ConvertBack(false, typeof(FrameworkElement), null, null), null);
+        }
+    }
+}
