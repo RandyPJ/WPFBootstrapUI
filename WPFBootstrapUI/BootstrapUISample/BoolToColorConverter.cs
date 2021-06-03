3d15570a0c370d46dc7c8b82b01929ca9b9e39b5
Randy Pena Jimenez <randy_li25@hotmail.com>
2021-06-03 11:33:59 -0400

    Delete git ignored files

--- WPFBootstrapUI/BootstrapUISample/BoolToColorConverter.cs
+++ WPFBootstrapUI/BootstrapUISample/BoolToColorConverter.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
-using System.Windows.Media;
-
-namespace BootstrapUISample
+﻿namespace BootstrapUISample
 {
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Media;
+
     public class BoolToColorConverter : IValueConverter 
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
